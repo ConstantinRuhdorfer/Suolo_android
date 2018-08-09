2dd77f904ab71c3bc79e6f9eea3e4d35106d9d3c
Constantin Ruhdorfer <constantin.ruhdorfer@diconium.com>
2018-08-09 09:51:23 +0200

    Intital commit

---
+++ app/src/main/java/com/ruhdocon/digitalfarming_tbd_ph/PH_Presenter_MainActivity.java
@@ -0,0 +1,13 @@
+package com.ruhdocon.digitalfarming_tbd_ph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PH_Presenter_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ph__presenter__main);
+    }
+}
